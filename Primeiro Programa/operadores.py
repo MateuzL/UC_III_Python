ca6f479a5b20b921315455ffbf727d66465f7638
MateuzL <mateuzl.dev02@outlook.com>
2026-04-24 20:59:49 -0400

    Inicio python
    Operadores, Variáveis, Entrada de Dados e conversão de Dados.

---
+++ Primeiro Programa/operadores.py
@@ -0,0 +1,29 @@
+#OPERADORES MATEMATICOS
+#ADIÇÃO
+#print(4+2)
+#SUBTRAÇÃO
+#print(4-2)
+#MULTIPLICAÇÃO
+#print(4*2)
+#DIVISÃO
+#print(4/2)
+#QUOCIENTE
+#print(4//2)
+#RESTO DA DIVISÃO
+#print(4%2)
+#POTENCIAÇÃO
+#print(4**2)
+
+#OPERAÇÃO COM VARIAVEIS
+#num1 = 10
+#num2 = 20
+#print(num1 + num2)
+
+#Abreviação de OPERADORES
+x = 2
+print(x) # Aqui ele imprime o valor de 2
+#x = x + x
+x += 4
+print(x) # Aqui ele imprime a soma da variavel + 4 = 6
+x -= 2
+print(x) #Resultado = 4
---
+++ variavel.py
@@ -0,0 +1,14 @@
+#Aprendendo a criar VARIAVEIS
+nome = "Mateus"    #STRING
+idade = 24         #INT
+salario = 220.20   #FLOAT
+vivo = True        #BOOLEAN
+
+print(nome)
+print(idade)
+print(salario)
+print(vivo)
+
+#Função apra descobrir o tipo da variavel
+print(type(nome))
+print(type(idade))
