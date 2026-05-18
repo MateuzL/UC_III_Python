621c7f51700405e5b258bcc4e61bcdf63d29953c
MateuzL <mateuzl.dev02@outlook.com>
2026-05-18 18:41:01 -0400

    Update exercicio13.py

--- Estrutura Repetição/exercicio13.py
+++ Estrutura Repetição/exercicio13.py
@@ -18,9 +18,7 @@ while True:
         disciplina = "Português"
     elif disciplina == 3:
         disciplina = "História"
-    else:
-        print("Disciplina inválida. Tente novamente.")
-        break
+        
     nome = input("Digite o nome do aluno: ")
     idade = int(input("Digite a idade do aluno: "))
     sexo = input("Digite o sexo do aluno: ")
